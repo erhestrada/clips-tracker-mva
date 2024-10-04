00376b70c5939c47ad0297061166743108262321
Erik <180784081+erhetrada@users.noreply.github.com>
2024-10-04 17:27:19 -0400

    only display english clips

--- main.js
+++ main.js
@@ -15,30 +15,35 @@ async function getTopClips(clientId, authToken) {
       const embedUrls = clipsData.data.map((datum) => datum.embed_url)
       const thumbnailUrls = clipsData.data.map((datum) => datum.thumbnail_url)
       const titles = clipsData.data.map((datum) => datum.title)
-      //console.log(clipsData.data);
-      console.log(embedUrls);
-      console.log(titles);
+      const languages = clipsData.data.map((datum) => datum.language)
+      console.log(clipsData.data);
+      //console.log(embedUrls);
+      //console.log(titles);
+      console.log(languages)
       //console.log(embedUrls[0])
       const parentElement = document.body;
 
       thumbnailUrls.forEach((url, index) => {
-        const iframe = document.createElement('iframe');
-        iframe.src = url + "&parent=localhost";
-        iframe.height = 360;
-        iframe.width = 640;
-        iframe.frameBorder = 0;
-        iframe.allow = 'autoplay *; encrypted-media *;';
-        iframe.loading = 'lazy';
-        iframe.allowFullscreen = true;
+        if(languages[index] === 'en') {
+            const iframe = document.createElement('iframe');
+            iframe.src = url + "&parent=localhost";
+            iframe.height = 360;
+            iframe.width = 640;
+            iframe.frameBorder = 0;
+            iframe.allow = 'autoplay *; encrypted-media *;';
+            iframe.loading = 'lazy';
+            iframe.allowFullscreen = true;
+    
+            const titleElement = document.createElement('p');
+            titleElement.textContent = titles[index];
+            
+            const imageContainer = document.createElement('div');
+            imageContainer.appendChild(iframe);
+            imageContainer.appendChild(titleElement);
+          
+            parentElement.appendChild(imageContainer);
+        }
 
-        const titleElement = document.createElement('p');
-        titleElement.textContent = titles[index];
-        
-        const imageContainer = document.createElement('div');
-        imageContainer.appendChild(iframe);
-        imageContainer.appendChild(titleElement);
-      
-        parentElement.appendChild(imageContainer);
       });
   
       
