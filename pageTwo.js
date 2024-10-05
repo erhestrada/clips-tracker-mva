9abdd6d266a42b9141af6248c7207df37d2f6382
Erik <180784081+erhetrada@users.noreply.github.com>
2024-10-04 20:33:36 -0400

    refactor pageTwo.js to remove unnecessary async-await

--- pageTwo.js
+++ pageTwo.js
@@ -1,14 +1,4 @@
-async function displayClip(clientId, authToken) {
-    try {
-      const response = await fetch("https://api.twitch.tv/helix/clips?game_id=509658&started_at=2024-10-03T16:36:58Z&ended_at=2024-10-04T16:36:58Z&is_featured=true", {
-        method: 'GET',
-        headers: {
-          'Client-Id': clientId,
-          'Authorization': 'Bearer ' + authToken
-        }
-      });
-      const clipsData = await response.json();
-
+function displayClip(clientId, authToken) {
       const urlParams = new URLSearchParams(window.location.search);
       const paramValue = urlParams.get('index');
       console.log(paramValue);
@@ -25,12 +15,7 @@ async function displayClip(clientId, authToken) {
       iframe.frameBorder = 0;
       iframe.allowFullscreen = true;
     
-      document.body.appendChild(iframe);
-            
-      return clipsData;
-    } catch (error) {
-      console.error(error);
-    }
+      document.body.appendChild(iframe);        
   }
   
   const data = displayClip(clientId, authToken)
